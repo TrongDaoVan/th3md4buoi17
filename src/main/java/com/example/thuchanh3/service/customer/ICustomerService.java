ea4c425fc87c19e1de830903b40d628e4da7947b
Dao Van Trong <daovantrong98@gmail.com>
2022-09-29 08:23:00 +0700

    all

---
+++ src/main/java/com/example/thuchanh3/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.thuchanh3.service.customer;
+
+import com.example.thuchanh3.model.Customer;
+import com.example.thuchanh3.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
